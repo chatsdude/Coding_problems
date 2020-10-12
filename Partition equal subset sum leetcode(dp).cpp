bae933809664cd78975a5662c3910ba0b5dbb5c8
chatsdude <41889572+chatsdude@users.noreply.github.com>
2020-10-12 21:05:55 +0530

    Update and rename Partition equal subset sum leetcode(dp) to Partition equal subset sum leetcode(dp).cpp

--- Partition equal subset sum leetcode(dp).cpp
+++ Partition equal subset sum leetcode(dp).cpp
@@ -1,4 +1,4 @@
-'''Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.'''
+/*Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.*/
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
